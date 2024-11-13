cc024e5c741cd0f47860c6f6a97a33381cd87655
Marta Hilgert <marta.hil.1988@gmail.com>
2024-11-13 17:58:32 -0300

    .createTaskDiv & .taskAddInput - styles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header"
 import { Input } from "./components/Input"
 
+import styles from './App.module.css'
 import './global.css'
 
 function App() {
@@ -10,7 +11,9 @@ function App() {
       <Header />
 
       <section>
-        <Input />
+        <div className={styles.createTaskDiv}>
+          <Input className={styles.taskAddInput}/>
+        </div>
       </section>
     </main>
   )

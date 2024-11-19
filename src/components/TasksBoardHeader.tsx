caadfb0d0c3f42cd682cf1e57e436d3ab7071554
Marta Hilgert <marta.hil.1988@gmail.com>
2024-11-19 16:53:50 -0300

    TasksBoardHeader - styles added

--- src/components/TasksBoardHeader.tsx
+++ src/components/TasksBoardHeader.tsx
@@ -3,15 +3,15 @@ import styles from './TasksBoardHeader.module.css'
 export function TasksBoardHeader() {
   return (
     <div className={styles.boardHeader}>
-      <div className={styles.createAndCompletedTasks}>
+      <div className={styles.createdAndCompletedTasks}>
         <div className={styles.createdTasksDiv}>
           <span className={styles.createdTasksTxt}>Created tasks</span>
-          <span className={styles.createdTasksCounter}></span>
+          <span className={styles.createdTasksCounter}>5</span>
         </div>
 
         <div className={styles.completedTasksDiv}>
           <span className={styles.completedTasksTxt}>Completed tasks</span>
-          <span className={styles.completedTasksCounter}></span>
+          <span className={styles.completedTasksCounter}>2 of 5</span>
         </div>
       </div>
     </div>
